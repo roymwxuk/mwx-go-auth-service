b8e76ae045a9c3ffec9bd1630d533ce0de85cd7e
roymwxuk <roymwxuk@gmail.com>
2026-08-22 02:01:50 +0900

    Implement JWT access token generation with claims

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,5 +1,11 @@
 package auth
 
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
 type JWTService struct {
 	secret string
 }
@@ -10,9 +16,27 @@ func NewJWTService(secret string) *JWTService {
 	}
 }
 
-func (j *JWTService) GenerateAccessToken(user *User) (string, error) {
-	// TBD: implement JWT generation logic here
-	return "dummy_access_token", nil
+func (j *JWTService) GenerateAccessToken(
+	user *User,
+) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": user.ID.String(),
+		"email":   user.Email,
+
+		"iat": time.Now().Unix(),
+		"exp": time.Now().
+			Add(15 * time.Minute).
+			Unix(),
+	}
+
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		claims,
+	)
+
+	return token.SignedString(
+		[]byte(j.secret),
+	)
 }
 
 func (j *JWTService) GenerateRefreshToken(user *User) (string, error) {
