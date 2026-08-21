452e4e1a82596161af0cafe337d4ece0167c51bf
roymwxuk <roymwxuk@gmail.com>
2026-08-22 05:16:56 +0900

    feat: implement VerifyAccessToken()

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,10 +1,13 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
+
+var signingMethod = jwt.SigningMethodHS256
 
 type JWTService struct {
 	secret string
@@ -31,7 +34,7 @@ func (j *JWTService) GenerateAccessToken(
 	}
 
 	token := jwt.NewWithClaims(
-		jwt.SigningMethodHS256,
+		signingMethod,
 		claims,
 	)
 
@@ -52,7 +55,7 @@ func (j *JWTService) GenerateRefreshToken(user *User) (string, error) {
 	}
 
 	token := jwt.NewWithClaims(
-		jwt.SigningMethodHS256,
+		signingMethod,
 		claims,
 	)
 
@@ -64,9 +67,54 @@ func (j *JWTService) GenerateRefreshToken(user *User) (string, error) {
 	return tokenString, nil
 }
 
-func (j *JWTService) VerifyAccessToken(token string) (*User, error) {
-	// TBD: implement JWT verification logic here
-	return nil, nil
+type AccessTokenClaim struct {
+	UserID string
+	Email  string
+}
+
+func (j *JWTService) VerifyAccessToken(
+	tokenString string,
+) (*AccessTokenClaim, error) {
+	keyFunc := func(token *jwt.Token) (any, error) {
+		if token.Method != signingMethod {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(j.secret), nil
+	}
+	token, err := jwt.Parse(tokenString, keyFunc)
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, errors.New("invalid claims")
+	}
+
+	userID, ok := claims["user_id"].(string)
+	if !ok {
+		return nil, errors.New("missing user_id")
+	}
+
+	email, ok := claims["email"].(string)
+	if !ok {
+		return nil, errors.New("missing email")
+	}
+
+	tokenType, ok := claims["type"].(string)
+	if !ok || tokenType != "access" {
+		return nil, errors.New("invalid token type")
+	}
+
+	return &AccessTokenClaim{
+		UserID: userID,
+		Email:  email,
+	}, nil
 }
 
 func (j *JWTService) AccessTokenExpiryInSec() int {
