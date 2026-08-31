fbbbe39bbc5852283a7ac16fcd0c28ffe54c1dd9
roymwxuk <roymwxuk@gmail.com>
2026-08-31 13:10:59 +0900

    fix: update health check message to include version information

--- internal/httpapi/health.go
+++ internal/httpapi/health.go
@@ -9,6 +9,6 @@ import (
 func HealthHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
-		"msg":    "mw-auth-service is running",
+		"msg":    "mw-auth-service is running. version: 1.0.1",
 	})
 }
